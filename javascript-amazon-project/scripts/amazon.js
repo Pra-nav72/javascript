40ebe3ff7c21956e8970fea6947928c33414c812
Pranav Kumar <Gyannik7772@gmail.com>
2026-04-04 18:10:29 +0530

    make cart quantity interactive

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -112,6 +112,12 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
             });
         }
 
-        console.log(cart);
+        let cartQuantity = 0;
+        cart.forEach((item) => {
+          cartQuantity += item.quantity;
+        })
+
+        // showing the no. of item in cart
+        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
     });
 });
