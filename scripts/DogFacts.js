48d7f47fa49a2b5a8ec459d22518a14cdb6b676f
Pranav Kumar <Gyannik7772@gmail.com>
2026-04-11 17:12:59 +0530

    filename modifications

--- scripts/DogFacts.js
+++ scripts/DogFacts.js
@@ -13,6 +13,9 @@ async function fetchData(){
     try{
         const response = await fetch(URL+`${randomNum()}`);
         if(!response.ok){
+            textbox.classList.remove("hidden");
+            textbox.classList.add("error");
+            textbox.textContent = `Error! status: ${response.status}`;
             throw new error(`HTTP error! status: ${response.status}`);
         }
         const data = response.json();
